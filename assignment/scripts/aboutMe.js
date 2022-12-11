8aac45971dc1e988a457f4278df6e9542d60e5bf
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2022-12-11 17:07:09 -0600

    added fullname and lucky number

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -3,13 +3,18 @@
 let firstName = 'Sean';
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+let lastName = 'Kimbrough';
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
+let fullName = 'firstName' + 'lastName'; 
 
 // 4 - Console log the value of `fullName`
+console.log( 'fullName is' + firstName + lastName);
+
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
+let luckyNumber = 24
 
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
